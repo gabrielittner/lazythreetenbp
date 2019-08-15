00c123aa9051333ccac429a09615fabac3409d61
Gabriel Ittner <gabriel.ittner@gmail.com>
2019-08-15 14:42:01 +0200

    update timezone database to version 2019b

--- runtime/src/main/java/com/gabrielittner/threetenbp/LazyZoneRules.java
+++ runtime/src/main/java/com/gabrielittner/threetenbp/LazyZoneRules.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 final class LazyZoneRules {
-  static final String VERSION = "2019a";
+  static final String VERSION = "2019b";
 
   static final List<String> REGION_IDS = Arrays.asList(
       "Africa/Abidjan",
